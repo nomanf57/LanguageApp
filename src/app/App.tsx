46dac66088e8d96fee2fb304191101ceeb8abf6f
Noman Rafique <nomrafique@gmail.com>
2022-01-23 19:51:20 +0500

    (fix): eslint duplicate files

---
+++ src/app/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import type { ReactNode } from 'react';
+import { Text } from 'react-native';
+
+const App: () => ReactNode = () => <Text>Hello</Text>;
+
+export default App;
